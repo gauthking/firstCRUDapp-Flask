4e5593cf7af7def326eb7a62397b4515e691939e
Gautham <appskans2017@gmail.com>
2022-04-24 00:26:55 +0300

    created flask app and made my first db

---
+++ app.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///myTodo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    cont = db.Column(db.String(600), nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route("/", methods=['GET', 'POST'])
+def helloworld():
+    if request.method == 'POST':
+        title1 = request.form['title']
+        cont1 = request.form['content']
+        todo = Todo(title=title1, cont=cont1)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo = allTodo)
+
+
+@app.route('/show')
+def show():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'this is the db page'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
